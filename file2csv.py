73d3a2af069840da7f2d70119fa36e820874c40e
Jake Nelson <couldbejake@gmail.com>
2020-08-04 03:29:26 +0100

    Update file2csv.py
    ACCESSIBILITY UPDATE 1 - CODE STYLING

--- file2csv.py
+++ file2csv.py
@@ -13,28 +13,28 @@ import sys
 import time
 import csv
 
-def convertToCSV(input_filenames, output_filename, lines_to_convert):
+def convertToCSV( input_filenames, output_filename, lines_to_convert ):
     
-    f_out = open(output_filename, "w")
-    f_out.write(','.join(map(str, input_filenames)) + "\n")
+    f_out = open( output_filename, "w" )
+    f_out.write( ','.join( map( str, input_filenames ) ) + "\n" )
 
-    csv_data = {}
+    csv_data = { }
 
     for file_name in input_filenames:
         column_name = file_name
 
-        column_data = {}
+        column_data = { }
 
         i = 0
-        for line in open(file_name, "r"):
-            column_data[i] = "\""+line.replace("\\", "")+"\""
+        for line in open( file_name, "r" ):
+            column_data[ i ] = "\"" + line.replace("\\", "") + "\""
             i += 1
 
-        csv_data[column_name] = column_data
+        csv_data[ column_name ] = column_data
 
-    if(lines_to_convert == -1):
-        first_column = input_filenames[0]
-        line_count = len(csv_data[first_column])
+    if( lines_to_convert == -1 ):
+        first_column = input_filenames[ 0 ]
+        line_count = len( csv_data[ first_column ] )
     else:
         line_count = lines_to_convert
 
@@ -43,9 +43,9 @@ def convertToCSV(input_filenames, output_filename, lines_to_convert):
         line_data = []
         
         for column_name in input_filenames:
-            line_data.append(csv_data[column_name][i].replace("\n", ""))
+            line_data.append( csv_data[ column_name ][ i ].replace( "\n", "" ) )
 
-        f_out.write(','.join(map(str, line_data)) +"\n")
+        f_out.write( ','.join( map( str, line_data ) ) + "\n" )
 
     f_out.close()
 
@@ -53,10 +53,10 @@ def convertToCSV(input_filenames, output_filename, lines_to_convert):
 
 if __name__ == "__main__":
     
-    parser=argparse.ArgumentParser()
-    parser.add_argument('--input', nargs='*', metavar=('input file name(s)'),help='A list of input file names.', required=True)
-    parser.add_argument('--linecount', nargs=1, metavar=('lines to convert'),help='The amount of lines that should be converted.')
-    parser.add_argument('--output', nargs=1, metavar=('output file name'),help='The file the converter should output to.')
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '--input', nargs = '*', metavar=( 'input file name(s)'), help='A list of input file names.', required = True)
+    parser.add_argument( '--linecount', nargs = 1, metavar=( 'lines to convert'), help='The amount of lines that should be converted.' )
+    parser.add_argument( '--output', nargs = 1, metavar=( 'output file name'), help='The file the converter should output to.' )
 
     args = parser.parse_args()
 
@@ -65,38 +65,38 @@ if __name__ == "__main__":
     input_filenames = args.input
 
     if(args.output != None):
-        output_filename = args.output[0]
+        output_filename = args.output[ 0 ]
     else:
         output_filename = "output.csv"
     
-    lines_to_convert = -1
+    lines_to_convert = - 1
 
     input_files_exist = True
 
     for input_filename in input_filenames:
-        if(not os.path.exists(input_filename)):
+        if( not os.path.exists( input_filename ) ):
             input_files_exist = False
 
-    if(not input_files_exist):
+    if( not input_files_exist ):
         print("One or more of the files you have specified in the arguments do not exist!")
         sys.exit()
 
-    if(args.linecount != None):
+    if( args.linecount != None ):
         try:
-            lines_to_convert = int(args.linecount[0])
+            lines_to_convert = int( args.linecount[ 0 ] )
             
-            if(lines_to_convert < 0):
+            if( lines_to_convert < 0 ):
                 
                 print("Lines to convert must be a positive integer!")
                 
         except ValueError:
             
-            print("Line count must be an integer!")
+            print( "Line count must be an integer!" )
             
             sys.exit()
 
-    print("Loading data from input files '"+', '.join(map(str, input_filenames))+"' into '"+output_filename+"'")
+    print( "Loading data from input files '"+', '.join( map( str, input_filenames ) )+ "' into '" + output_filename + "'" )
     
-    converted_line_count = convertToCSV(input_filenames, output_filename, lines_to_convert)
+    converted_line_count = convertToCSV( input_filenames, output_filename, lines_to_convert )
 
-    print("(Succesfully converted "+str(converted_line_count)+" lines in " + str("{0:.2f}".format(time.time() - proc_start)) + " seconds!)")
+    print("(Succesfully converted "+str( converted_line_count ) + " lines in " + str( "{0:.2f}".format( time.time() - proc_start) ) + " seconds! )")
